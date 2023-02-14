14de1cdd11243f642a28aa8b3eabe7efdc933049
martinhpcodecnoob <hp.u.smile@gmail.com>
2023-02-13 22:30:10 -0500

    Pruebas con APISearch25

--- api/src/routes/getSearch.js
+++ api/src/routes/getSearch.js
@@ -9,48 +9,49 @@ router.get('/:buscar',async(req,res)=>{
     try {
         let comprobarBDsearch = await Search.findAll()
         const busquedaTotal = await BusquedaApi(buscar)
-        if (comprobarBDsearch.length < 1) {
-            busquedaTotal.forEach(async(element,i) => {
-                await Search.findOrCreate({
-                    where:{
-                        id:i,
-                        category:element.category,
-                        name:element.name,
-                        author: element.author,
-                        title: element.title,
-                        description: element.description,
-                        url: element.url,
-                        urlToImage: element.urlToImage,
-                        publishedAt: element.publishedAt,
-                    }
-                })
-            });
-            let newResult = await Search.findAll()
-            res.send(newResult)
+        res.send(busquedaTotal)
+        // if (comprobarBDsearch.length < 1) {
+        //     busquedaTotal.forEach(async(element,i) => {
+        //         await Search.findOrCreate({
+        //             where:{
+        //                 id:i,
+        //                 category:element.category,
+        //                 name:element.name,
+        //                 author: element.author,
+        //                 title: element.title,
+        //                 description: element.description,
+        //                 url: element.url,
+        //                 urlToImage: element.urlToImage,
+        //                 publishedAt: element.publishedAt,
+        //             }
+        //         })
+        //     });
+        //     let newResult = await Search.findAll()
+        //     res.send(newResult)
 
-        const busquedaTotal = await BusquedaApi(buscar)
-        }else{
-            await Search.destroy({
-                truncate:true
-            })
-            busquedaTotal.forEach(async(element,i) => {
-                await Search.findOrCreate({
-                    where:{
-                        id:i,
-                        category:element.category,
-                        name:element.name,
-                        author: element.author,
-                        title: element.title,
-                        description: element.description,
-                        url: element.url,
-                        urlToImage: element.urlToImage,
-                        publishedAt: element.publishedAt,
-                    }
-                })
-            });
-            let newResult = await Search.findAll()
-            res.send(newResult)
-        }
+        // const busquedaTotal = await BusquedaApi(buscar)
+        // }else{
+        //     await Search.destroy({
+        //         truncate:true
+        //     })
+        //     busquedaTotal.forEach(async(element,i) => {
+        //         await Search.findOrCreate({
+        //             where:{
+        //                 id:i,
+        //                 category:element.category,
+        //                 name:element.name,
+        //                 author: element.author,
+        //                 title: element.title,
+        //                 description: element.description,
+        //                 url: element.url,
+        //                 urlToImage: element.urlToImage,
+        //                 publishedAt: element.publishedAt,
+        //             }
+        //         })
+        //     });
+        //     let newResult = await Search.findAll()
+        //     res.send(newResult)
+        // }
     } catch (error) {
         res.status(400).send({error:error})
     }
