4f9104bbcdde363a63f19830132715dfd8c52be8
martinhpcodecnoob <hp.u.smile@gmail.com>
2023-02-13 22:20:38 -0500

    Pruebas con APISearch24

--- api/src/routes/getSearch.js
+++ api/src/routes/getSearch.js
@@ -7,60 +7,50 @@ const router = Router();
 router.get('/:buscar',async(req,res)=>{
     const {buscar} = req.params;
     try {
-        // let comprobarBDsearch = await Search.findAll()
-        // const busquedaTotal = await BusquedaApi(buscar)
-        // if (comprobarBDsearch.length < 1) {
-        //     busquedaTotal.forEach(async(element,i) => {
-        //         await Search.findOrCreate({
-        //             where:{
-        //                 id:i,
-        //                 category:element.category,
-        //                 name:element.name,
-        //                 author: element.author,
-        //                 title: element.title,
-        //                 description: element.description,
-        //                 url: element.url,
-        //                 urlToImage: element.urlToImage,
-        //                 publishedAt: element.publishedAt,
-        //             }
-        //         })
-        //     });
-        //     let newResult = await Search.findAll()
-        //     res.send(newResult)
-
-        // const busquedaTotal = await BusquedaApi(buscar)
-        
+        let comprobarBDsearch = await Search.findAll()
         const busquedaTotal = await BusquedaApi(buscar)
-        if (busquedaTotal.length < 1) {
-            res.status(400).send({error:"Algo anda mal"})
+        if (comprobarBDsearch.length < 1) {
+            busquedaTotal.forEach(async(element,i) => {
+                await Search.findOrCreate({
+                    where:{
+                        id:i,
+                        category:element.category,
+                        name:element.name,
+                        author: element.author,
+                        title: element.title,
+                        description: element.description,
+                        url: element.url,
+                        urlToImage: element.urlToImage,
+                        publishedAt: element.publishedAt,
+                    }
+                })
+            });
+            let newResult = await Search.findAll()
+            res.send(newResult)
+
+        const busquedaTotal = await BusquedaApi(buscar)
         }else{
-            res.send(busquedaTotal)
+            await Search.destroy({
+                truncate:true
+            })
+            busquedaTotal.forEach(async(element,i) => {
+                await Search.findOrCreate({
+                    where:{
+                        id:i,
+                        category:element.category,
+                        name:element.name,
+                        author: element.author,
+                        title: element.title,
+                        description: element.description,
+                        url: element.url,
+                        urlToImage: element.urlToImage,
+                        publishedAt: element.publishedAt,
+                    }
+                })
+            });
+            let newResult = await Search.findAll()
+            res.send(newResult)
         }
-
-        // }else{
-        //     await Search.destroy({
-        //         truncate:true
-        //     })
-        //     busquedaTotal.forEach(async(element,i) => {
-        //         await Search.findOrCreate({
-        //             where:{
-        //                 id:i,
-        //                 category:element.category,
-        //                 name:element.name,
-        //                 author: element.author,
-        //                 title: element.title,
-        //                 description: element.description,
-        //                 url: element.url,
-        //                 urlToImage: element.urlToImage,
-        //                 publishedAt: element.publishedAt,
-        //             }
-        //         })
-        //     });
-        //     let newResult = await Search.findAll()
-        //     res.send(newResult)
-        // }
-
-
     } catch (error) {
         res.status(400).send({error:error})
     }
